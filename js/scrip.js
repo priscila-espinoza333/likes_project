1ad131b6a8d6604eb026890ebe167bc762775da3
priscila-espinoza333 <pr.espinoza@duocuc.cl>
2022-08-30 03:01:19 -0400

    js

---
+++ js/scrip.js
@@ -0,0 +1,11 @@
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
